25d03067cd38afc769457e5798693c57f85dc084
akazeykina <anna.kazeykina@free.fr>
2020-12-02 09:51:27 +0100

    Added savefig

--- tests/plot_densities_masks.py
+++ tests/plot_densities_masks.py
@@ -5,11 +5,10 @@ Created on Fri Sep 25 21:29:27 2020
 
 @author: Anna
 """
-
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from densities_calculation.mask import compute_mask
@@ -17,8 +16,6 @@ from densities_calculation.s_distribution import avg_s_distribution
 from densities_calculation.calculate_densities import pi_rad, calculate_pi_blocks, unravel_pi
 from densities_calculation.generate_scheme import generate_full_scheme, generate_blocks_list, num_samples
 from densities_calculation.utils import extract_images, reduce_img_size
-
-
 
 
 img_size = 32
@@ -73,14 +70,27 @@ for pi_type in dens_type:
     pi_mask[ pi_type ] = compute_mask( pi[ pi_type ], nb_samples )
 #
 
+
+##############################################################################
+
+######## Create directory for pictures
+
+script_dir = os.path.dirname( __file__ )
+results_dir = os.path.join( script_dir, 'pictures/plot_densities_masks/' )
+if not os.path.isdir( results_dir ):
+    os.makedirs( results_dir )
+
+
+
 ###### Plot MRI images
 fig = plt.figure( figsize = ( 30, 5 ) )
 
 for i in range( 5 ):
     ax = fig.add_subplot(1, 5, i + 1 )
     plt.imshow( reduce_img_size( img_size, img_s_distrib_list[ i * 2 + 1 ] ), cmap = 'gray' )
-
-plt.show()
+    
+plt.savefig( results_dir+'fastmri_images.png', bbox_inches='tight')
+#plt.show()
 
 ###### Plot densities
 
@@ -97,7 +107,9 @@ for i, pi_type in enumerate( dens_type ):
     ax.tricontourf( full_kspace[ :, 1 ], full_kspace[ :, 0 ], pi_fl[ pi_type ],
                vmin = val_min, vmax = val_max )
     ax.set_aspect( 'equal' )
-plt.show()
+    
+plt.savefig( results_dir+'densities.png', bbox_inches='tight')
+#plt.show()
 #
 #
 ####### Plots masks
@@ -109,5 +121,6 @@ for i, pi_type in enumerate( dens_type ):
     ax = fig.add_subplot(1, 5, i + 1 )
     plt.imshow(pi_mask[ pi_type ], cmap='gray' )
     
-plt.show()
+plt.savefig( results_dir+'masks.png', bbox_inches='tight')
+#plt.show()
 #
