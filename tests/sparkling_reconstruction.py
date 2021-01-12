a4709249396e817c09729edf257529316093a9d0
akazeykina <anna.kazeykina@free.fr>
2021-01-12 15:28:58 +0100

    Added save kspace points

---
+++ tests/sparkling_reconstruction.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 15:11:21 2021
+
+@author: Anna
+"""
+
+kspace_loc = np.load( "kpoints/sparkling_"+pi_type+"_"+str(img_size)+".npy" )
